ffc07d08b9599155e8834df79982d04025915764
Yonas Kolb <yonaskolb@gmail.com>
2018-04-29 19:35:48 +1000

    clean up AnyCodable

--- Sources/Codability/AnyCodable.swift
+++ Sources/Codability/AnyCodable.swift
@@ -15,7 +15,7 @@ import Foundation
  - SeeAlso: `AnyEncodable`
  - SeeAlso: `AnyDecodable`
  */
-public struct AnyCodable: Codable {
+public struct AnyCodable {
     public let value: Any
     
     public init<T>(_ value: T?) {
@@ -23,7 +23,80 @@ public struct AnyCodable: Codable {
     }
 }
 
-extension AnyCodable: _AnyEncodable, _AnyDecodable {}
+extension AnyCodable: Codable {
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if container.decodeNil() {
+            self.init(())
+        } else if let bool = try? container.decode(Bool.self) {
+            self.init(bool)
+        } else if let int = try? container.decode(Int.self) {
+            self.init(int)
+        } else if let uint = try? container.decode(UInt.self) {
+            self.init(uint)
+        } else if let double = try? container.decode(Double.self) {
+            self.init(double)
+        } else if let string = try? container.decode(String.self) {
+            self.init(string)
+        } else if let array = try? container.decode([AnyCodable].self) {
+            self.init(array.map { $0.value })
+        } else if let dictionary = try? container.decode([String: AnyCodable].self) {
+            self.init(dictionary.mapValues { $0.value })
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "AnyCodable value cannot be decoded")
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch self.value {
+        case is Void:
+            try container.encodeNil()
+        case let bool as Bool:
+            try container.encode(bool)
+        case let int as Int:
+            try container.encode(int)
+        case let int8 as Int8:
+            try container.encode(int8)
+        case let int16 as Int16:
+            try container.encode(int16)
+        case let int32 as Int32:
+            try container.encode(int32)
+        case let int64 as Int64:
+            try container.encode(int64)
+        case let uint as UInt:
+            try container.encode(uint)
+        case let uint8 as UInt8:
+            try container.encode(uint8)
+        case let uint16 as UInt16:
+            try container.encode(uint16)
+        case let uint32 as UInt32:
+            try container.encode(uint32)
+        case let uint64 as UInt64:
+            try container.encode(uint64)
+        case let float as Float:
+            try container.encode(float)
+        case let double as Double:
+            try container.encode(double)
+        case let string as String:
+            try container.encode(string)
+        case let date as Date:
+            try container.encode(date)
+        case let url as URL:
+            try container.encode(url)
+        case let array as [Any?]:
+            try container.encode(array.map { AnyCodable($0) })
+        case let dictionary as [String: Any?]:
+            try container.encode(dictionary.mapValues { AnyCodable($0) })
+        default:
+            let context = EncodingError.Context(codingPath: container.codingPath, debugDescription: "AnyCodable value cannot be encoded")
+            throw EncodingError.invalidValue(self.value, context)
+        }
+    }
+}
 
 extension AnyCodable: Equatable {
     public static func ==(lhs: AnyCodable, rhs: AnyCodable) -> Bool {
@@ -92,4 +165,37 @@ extension AnyCodable: CustomDebugStringConvertible {
     }
 }
 
-extension AnyCodable: ExpressibleByNilLiteral, ExpressibleByBooleanLiteral, ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, ExpressibleByStringLiteral, ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral {}
+extension AnyCodable: ExpressibleByNilLiteral, ExpressibleByBooleanLiteral, ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, ExpressibleByStringLiteral, ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral {
+
+    public init(nilLiteral: ()) {
+        self.init(nil as Any?)
+    }
+
+    public init(booleanLiteral value: Bool) {
+        self.init(value)
+    }
+
+    public init(integerLiteral value: Int) {
+        self.init(value)
+    }
+
+    public init(floatLiteral value: Double) {
+        self.init(value)
+    }
+
+    public init(extendedGraphemeClusterLiteral value: String) {
+        self.init(value)
+    }
+
+    public init(stringLiteral value: String) {
+        self.init(value)
+    }
+
+    public init(arrayLiteral elements: Any...) {
+        self.init(elements)
+    }
+
+    public init(dictionaryLiteral elements: (AnyHashable, Any)...) {
+        self.init(Dictionary<AnyHashable, Any>(elements, uniquingKeysWith: { (first, _) in first }))
+    }
+}
---
+++ Tests/CodabilityTests/AnyCodableTests.swift
@@ -0,0 +1,144 @@
+import XCTest
+@testable import Codability
+
+final class AnyContainerTests: XCTestCase {
+
+    let jsonString = """
+        {
+            "boolean": true,
+            "integer": 1,
+            "double": 3.2,
+            "string": "string",
+            "array": [1, 2, 3],
+            "nested": {
+                "a": "alpha",
+                "b": "bravo",
+                "c": "charlie"
+            }
+        }
+        """
+
+    let jsonValue: [String: Any] = [
+        "boolean": true,
+        "integer": 1,
+        "double": 3.2,
+        "string": "string",
+        "array": [1, 2, 3],
+        "nested": [
+            "a": "alpha",
+            "b": "bravo",
+            "c": "charlie"
+        ]
+    ]
+
+    lazy var anyJson = """
+    {
+        "dictionary": \(jsonString),
+        "array": [
+        "hello",
+        2,
+        true
+        ],
+        "value": true
+        }
+    """
+
+    func testDecoding() {
+        let json = jsonString.data(using: .utf8)!
+
+        let decoder = JSONDecoder()
+        let dictionary = try! decoder.decode([String: AnyCodable].self, from: json)
+
+        XCTAssertEqual(dictionary["boolean"]?.value as! Bool, true)
+        XCTAssertEqual(dictionary["integer"]?.value as! Int, 1)
+        XCTAssertEqual(dictionary["double"]?.value as! Double, 3.2)
+        XCTAssertEqual(dictionary["string"]?.value as! String, "string")
+        XCTAssertEqual(dictionary["array"]?.value as! [Int], [1, 2, 3])
+        XCTAssertEqual(dictionary["nested"]?.value as! [String: String], ["a": "alpha", "b": "bravo", "c": "charlie"])
+    }
+
+    func testEncoding() {
+        let dictionary: [String: AnyCodable] = [
+            "boolean": true,
+            "integer": 1,
+            "double": 3.2,
+            "string": "string",
+            "array": [1, 2, 3],
+            "nested": [
+                "a": "alpha",
+                "b": "bravo",
+                "c": "charlie"
+            ]
+        ]
+
+        let encoder = JSONEncoder()
+
+        let json = try! encoder.encode(dictionary)
+        let encodedJSONObject = try! JSONSerialization.jsonObject(with: json, options: []) as! NSDictionary
+
+        let expected = jsonString.data(using: .utf8)!
+        let expectedJSONObject = try! JSONSerialization.jsonObject(with: expected, options: []) as! NSDictionary
+
+        XCTAssertEqual(encodedJSONObject, expectedJSONObject)
+    }
+
+    func testCodingAny() throws {
+
+        let json = anyJson.data(using: .utf8)!
+
+        let decoder = JSONDecoder()
+        let object = try decoder.decode(Object.self, from: json)
+
+        func assert(_ object: Object) {
+            XCTAssertEqual(object.dictionary["boolean"] as! Bool, true)
+            XCTAssertEqual(object.dictionary["integer"] as! Int, 1)
+            XCTAssertEqual(object.dictionary["double"] as! Double, 3.2)
+            XCTAssertEqual(object.dictionary["string"] as! String, "string")
+            XCTAssertEqual(object.dictionary["array"] as! [Int], [1, 2, 3])
+            XCTAssertEqual(object.dictionary["nested"] as! [String: String], ["a": "alpha", "b": "bravo", "c": "charlie"])
+            XCTAssertEqual(object.array[0] as! String, "hello")
+            XCTAssertEqual(object.array[1] as! Int, 2)
+            XCTAssertEqual(object.array[2] as! Bool, true)
+            XCTAssertEqual(object.value as! Bool, true)
+        }
+        assert(object)
+
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(object)
+        let object2 = try decoder.decode(Object.self, from: data)
+        assert(object2)
+    }
+
+
+    static var allTests = [
+        ("testCodingAny", testCodingAny),
+        ]
+}
+
+struct Object: Codable {
+
+    let dictionary: [String: Any]
+    let array: [Any]
+    let value: Any
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        dictionary = try container.decode([String: Any].self, forKey: .dictionary)
+        array = try container.decode([Any].self, forKey: .array)
+        value = try container.decode(Any.self, forKey: .value)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(dictionary, forKey: .dictionary)
+        try container.encode(array, forKey: .array)
+        try container.encode(value, forKey: .value)
+    }
+
+    enum CodingKeys: CodingKey {
+        case dictionary
+        case value
+        case array
+    }
+}
