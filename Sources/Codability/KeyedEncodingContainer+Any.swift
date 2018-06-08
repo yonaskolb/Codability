f7a0b437d74aa65d020c28e8c277e4ec8531d408
Yonas Kolb <yonaskolb@gmail.com>
2018-06-08 11:27:33 +1000

    fix nill AnyCodable values being encoded as null

--- Sources/Codability/KeyedEncodingContainer+Any.swift
+++ Sources/Codability/KeyedEncodingContainer+Any.swift
@@ -4,6 +4,7 @@ import Foundation
 extension KeyedEncodingContainer {
 
     public mutating func encodeAnyIfPresent<T>(_ value: T?, forKey key: K) throws {
+        guard let value = value else { return }
         try encodeIfPresent(AnyCodable(value), forKey: key)
     }
 
